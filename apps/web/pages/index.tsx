52ff222673ef98a42367815d5d0993a3cf18623b
Bruno Paulino <bruno@bpaulino.com>
2022-05-30 08:49:52 +0200

    Rename packages

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { UltimateBox, UltimateButton } from 'ultimate-components'
+import { UltimateBox, UltimateButton } from '@ultimate-monorepo/core'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
